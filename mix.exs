07617ef8fc88c52f3b43e23ccb013bb13e99bda5
Libo Shen <liboooshen@gmail.com>
2024-12-18 00:13:02 +0000

    release v0.1.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Nexlm.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/LiboShen/nexlm"
   @description "A unified interface for interacting with various Large Language Model (LLM) providers"
 
